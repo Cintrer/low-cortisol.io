1a7e5c3ae9083456968433a7bafb9bb78cd840c5
Thomas <thomas.degardin@ynov.com>
2026-04-10 10:34:27 +0200

    low-cortisol

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Test");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Raylib_cs.Color.RayWhite);
+
+    Raylib.DrawText("Hello", 300, 280, 20, Raylib_cs.Color.Black);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
